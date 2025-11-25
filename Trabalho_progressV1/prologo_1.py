2bcb4e2925243dd2b72143c4665b9d729cf9e9bb
Gabriel <gferreiradossantos95@gmail.com>
2025-11-25 19:06:29 -0300

    ata

---
+++ Trabalho_progressV1/prologo_1.py
@@ -0,0 +1,46 @@
+cliente = {
+    "nome": input("Digite seu nome: "), 
+    "idade": int(input("Digite a sua idade: ")),        
+    "email": input("Digite seu e-mail: ") 
+}
+
+tabela_preco = {
+    "Fiat":{
+        "Strada":111990.0,
+        "Argos":84990.0,
+        "Uno Mille":42594.0,
+        "Toro":159490.0,
+        "Mobi":80990.0},
+    
+    "Hyundai":{
+        
+        "HB20":95790.0,
+        "Creta":149620.0,
+        "Elantra":87162.0,
+        "Sonata":75000.0,
+        "Tucson":150000.0},
+
+    "Volkswagen":{
+        "Polo":62802.0,
+        "T-Cross":155990.0,
+        "Nivus": 143490.0,
+        " Amarok":141701.0,
+        "Saveiro": 69019.0} 
+}
+    
+
+    
+carros_vender = [
+  
+]
+
+carros_alugar = [
+    ("BYD", "Dolphin"),
+    ("BYD", "Dolphin Mini"),
+    ("Toyota", "Corolla"),
+    ("Toyota", "Etios"),
+    ("Honda", "Civic"),
+    ("Jeep", "Renegade"),
+    ("Renault", "Duster"),
+    ("Renault", "Kwid")
+]
