9897d17828017ddc2ad78060f3cd07fbfce80fe2
gferreiradossantos95-cmyk <gferreiradossantos95@gmail.com>
2025-11-30 01:03:39 -0300

    lista_pytohn

---
+++ ListaPY/Ex01.py
@@ -0,0 +1,7 @@
+# Crie um programa que peça um número ao usuário e conte de 1 até esse número,
+#exibindo todos os valores.
+
+n = int(input("Digite um valor: "))
+
+for c in range(1,n+1):
+    print(c)
